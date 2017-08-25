fb0c4324c265b8d81fe4812fc7622ec0e68216d1
SanKlein <paklein3@gmail.com>
2017-08-25 08:52:53 -0700

    binary search tree

--- IntroductionToAlgorithms/Examples.js
+++ IntroductionToAlgorithms/Examples.js
@@ -440,3 +440,114 @@ var listDelete = function(L, x) {
     x.next.prev = x.prev;
   }
 };
+
+
+// binary search trees
+function inorderTreeWalk(x) {
+  if (x !== null) {
+    inorderTreeWalk(x.left);
+    console.log(x.key);
+    inorderTreeWalk(x.right);
+  }
+}
+
+function treeSearch(x, k) {
+  if (x === null || k == x.key) {
+    return x;
+  }
+  if (k < x.key) {
+    return treeSearch(x.left, k);
+  } else {
+    return treeSearch(x.right, k);
+  }
+}
+
+var iterativeTreeSearch = function(x, k) {
+  while (x !== null && k !== x.key) {
+    if (k < x.key) {
+      x = x.left;
+    } else {
+      x = x.right;
+    }
+    return x;
+  }
+};
+
+var treeMinimum = function(x) {
+  while (x.left !== null) {
+    x = x.left;
+  }
+  return x;
+};
+
+var treeMaximum = function(x) {
+  while (x.right !== null) {
+    x = x.right;
+  }
+  return x;
+};
+
+var treeSuccessor = function(x) {
+  if (x.right !== null) {
+    return treeMinimum(x.right);
+  }
+  var y = x.p;
+  while (y !== null && x == y.right) {
+    x = y;
+    y = y.p;
+  }
+  return y;
+};
+
+var treeInsert = function(T, z) {
+  var y = null;
+  var x = T.root;
+  while (x !== null) {
+    y = x;
+    if (z.key < x.key) {
+      x = x.left;
+    } else {
+      x = x.right;
+    }
+    z.p = y;
+    if (y == null) {
+      T.root = z;
+    } else if (z.key < y.key) {
+      y.left = z;
+    } else {
+      y.right = z;
+    }
+    return T;
+  }
+};
+
+var transplant = function(T, u, v) {
+  if (u.p === null) {
+    T.root = v;
+  } else if (u === u.p.left) {
+    u.p.left = v;
+  } else {
+    u.p.right = v;
+  }
+  if (v !== null) {
+    v.p = u.p;
+  }
+};
+
+var treeDelete = function(T, z) {
+  if (z.left === null) {
+    transplant(T, z, z.right);
+  } else if (z.right === null) {
+    transplant(T, z, z.left);
+  } else {
+    y = treeMinimum(z.right);
+    if (y.p !== z) {
+      transplant(T, y, y.right);
+      y.right = z.right;
+      y.right.p = y;
+    }
+    transplant(T, z, y);
+    y.left = z.left;
+    y.left.p = y;
+  }
+}
